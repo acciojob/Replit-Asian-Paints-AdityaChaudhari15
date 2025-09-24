a7fdec9ba106929a5e0ff3be5168975c33e5ed8b
AdityaChaudhari15 <159461162+AdityaChaudhari15@users.noreply.github.com>
2025-09-24 11:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let items = document.querySelectorAll(".grid-item");
+let submit = document.getElementById("change_button");
+let reset = document.getElementById("reset_button");
+
+submit.addEventListener("click",()=>{
+	items.forEach((cell)=>{
+		cell.style.backgroundColor ="transparent";
+	});
+	let block = document.getElementById('block_id').value;
+	let color = document.getElementById('colour_id').value;
+
+	let changeCell = document.getElementById(`${block}`)
+	changeCell.style.backgroundColor = color;
+})
+
+
+reset.addEventListener("click",()=>{
+	items.forEach((cell)=>{
+		cell.style.backgroundColor ="transparent";
+	});
+	});
